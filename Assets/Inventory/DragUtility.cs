c7685dfab08fadb1f18b8692798ee45991c84be2
Rica Feng <rfeng001@ucr.edu>
2017-04-27 19:59:45 -0700

    Inventory dragging workshop

---
+++ Assets/Inventory/DragUtility.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragUtility : Singleton<DragUtility>
+{
+    /// <summary>
+    /// Minimum distance allowed that an item card will lock onto a grid. Else, the card will return to its original position.
+    /// BUG: Not Working
+    /// </summary>
+    public float MinimumDragDistance = 1000000f;
+
+    private GridContainer LastContainer = null;
+    private int LastIndex = -1;
+
+    [HideInInspector]
+    public List<GridContainer> CurrentlyActiveContainers = new List<GridContainer>();
+
+    public void StartDrag(GameObject card)
+    {
+
+        LastContainer = card.transform.GetComponentInParent<GridContainer>();
+        //LastContainer = card.transform.parent.parent.parent.GetComponent<GridContainer>();
+        LastIndex = LastContainer.GetCardIndex(card);
+        card.transform.SetParent(GetComponentInParent<Canvas>().gameObject.transform);
+    }
+
+    public void Dragging(GameObject card)
+    {
+        card.transform.position = Input.mousePosition;
+        
+    }
+
+    public void EndDrag(GameObject card)
+    {
+        //Current lowest distance between grid and currently held card
+        float lowest_distance = MinimumDragDistance;
+
+        GridContainer container_final = null;
+
+        int new_index = -1;
+        //Check all active containers to detect where item should be placed
+        foreach (GridContainer container in CurrentlyActiveContainers)
+        {
+            for (int i = 0; i < container.CardMax; ++i)
+            {
+                //print("iterating " + i);
+                GameObject grid_space = container.GridDisplay.transform.GetChild(i).gameObject;
+                float curr_lowest = Vector2.Distance(grid_space.transform.position, card.transform.position);
+                if (curr_lowest < lowest_distance)
+                {
+                    lowest_distance = curr_lowest;
+                    container_final = container;
+                    new_index = i;
+                }
+            }
+        }
+
+        if (new_index != -1) //Switch
+        {
+            LastContainer.SwitchCards(LastIndex, card, container_final, new_index);
+        }
+        else // Do not switch
+        {
+            LastContainer.AddCard(card, LastIndex);
+        }
+    }
+}
---
+++ Assets/Inventory/GridContainer.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
+
+public class GridContainer : MonoBehaviour
+{
+
+    public GameObject GridDisplay
+    {
+        get { return gameObject; }
+    }
+
+    /// <summary>
+    /// Max amount of cards that can be stored
+    /// </summary>
+    public int CardMax
+    {
+        get
+        {
+            return GridDisplay.transform.childCount;
+        }
+    }
+
+    private void OnEnable()
+    {
+        //Debug.Log("enable " + name);
+        DragUtility.Instance.CurrentlyActiveContainers.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        //Debug.Log("disable " + name);
+        DragUtility.Instance.CurrentlyActiveContainers.Remove(this);
+    }
+
+    /// <summary>
+    /// Add physical card to grid display to specified index
+    /// </summary>
+    public void AddCard(GameObject card, int index)
+    {
+        if (index < 0 || index >= CardMax)
+            return;
+        else if (card == null)
+            return;
+
+        card.transform.SetParent(GridDisplay.transform.GetChild(index));
+    }
+
+    /// <summary>
+    /// Attempt to switch this container's card to the other container's index
+    /// </summary>
+    public void SwitchCards(int this_index, GameObject card, GridContainer container, int other_index)
+    {
+        //Debug.Log("from container " + this.name + " switch " + card.name + " in index " + this_index + " to container " + container.name + " at index " + other_index);
+        if (!ValidIndex(this_index))
+            return;
+        else if (!container.ValidIndex(other_index))
+            return;
+
+        GameObject other_card = container.GetCardFromIndex(other_index);
+        if (other_card != null) //card exists in other container
+        {
+            other_card.transform.SetParent(null);
+            AddCard(other_card, this_index);
+            container.AddCard(card, other_index);
+        }
+        else
+        {
+            container.AddCard(card, other_index);
+        }
+    }
+
+    /// <summary>
+    /// Returns true if this index is within range
+    /// </summary>
+    public bool ValidIndex(int index)
+    {
+        return index >= 0 && index < CardMax;
+    }
+
+    /// <summary>
+    /// Returns card that is located in the specified index
+    /// </summary>
+    public GameObject GetCardFromIndex(int index)
+    {
+        if (!ValidIndex(index))
+            return null;
+        else if (GridDisplay.transform.GetChild(index).childCount == 0)
+            return null;
+
+        return GridDisplay.transform.GetChild(index).GetChild(0).gameObject;
+    }
+
+    /// <summary>
+    /// If card is found in container, return its index
+    /// Otherwise, return -1
+    /// </summary>
+    public int GetCardIndex(GameObject card)
+    {
+        if (card == null)
+            return -1;
+
+        for (int i = 0; i < CardMax; ++i)
+        {
+            if (GridDisplay.transform.GetChild(i).childCount != 0)
+            {
+                if (GridDisplay.transform.GetChild(i).GetChild(0).gameObject == card)
+                    return i;
+            }
+        }
+        return -1;
+    }
+
+}
